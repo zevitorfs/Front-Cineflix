ad0473596907dd291b6419be27b782f12150d61b
zevitorfs <zetune910@gmail.com>
2024-12-05 18:15:50 -0300

    feat: :sparkles: Melhorias no banner agora um um gradiente e mais responsivel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,20 @@
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div className={`relative h-full lg:h-[140vh] ${inter.className}`}>   
-      <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
+    <div className='relative h-screen overflow-hidden bg-gradient-to-t from-black lg:h-[140vh]'>
+
+      <main className='relative pb-24 pl-4 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
           <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
-          <Image
-            src='/banner_strangerthing.jpg'
-            alt='Banner Stranger Things'
-            fill={true}
-            className ='h-[65vh] object-cover object-top lg:h-[95vh]'
-          />
-
+            <Image
+              src='/Banner_stragerthing.png'
+              alt='Stranger Things'
+              fill={true}
+              className='h-[65vh] object-cover object-top lg:h-[95vh]'
+            />
           </div>
-
-          
           <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
             Stranger Things
           </h1>
