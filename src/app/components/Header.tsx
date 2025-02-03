709d01de5c214f1c44a0426131c1adff26282aa4
zevitorfs <zetune910@gmail.com>
2025-02-03 16:28:12 -0300

    feat: :sparkles: feat: atualização na header quando da um scrool o background muda para preto

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+'use client';
+import React, { useEffect, useState } from "react";
 
 export default function Header(){
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handScrool = () => {
+            if(window.scrollY > 0){ // se o scrool gira ele vai fazer um registro
+                setIsScrolled(true);
+            }else{
+                setIsScrolled(false);
+            }
+        }; // vai retorna quando o usario fizer o scroll na pagina
+        
+        window.addEventListener('scroll', handScrool);
+
+        return () => window.removeEventListener('scroll', handScrool); // vai ser responsavel
+
+    }, []);
+
     return (   
-        <header className='fixed top-0 z-10 flex w-full itens-center justify-between transition-all px-4 py-4 lg:px-10 lg:py-6'>
+        <header className={` ${isScrolled && 'bg-black'} fixed top-0 z-10 flex w-full itens-center justify-between transition-all px-4 py-4 lg:px-10 lg:py-6`}>
             <div className='flex items-center space-x-2 md:space-x-8'>
                 <img src="/logo_netflix.png" alt="Netflix" width={120} height={120} />
                 <ul className='hidden md:flex md:space-x-4'>
